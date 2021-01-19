01db91381866e1118cff9ffb8541c3485801aea3
Ivan Kraljević <ivankraljevic177@gmail.com>
2021-01-20 00:41:53 +0100

    update sorting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,53 +1,69 @@
-import   { BrowserRouter as Router, Route} from 'react-router-dom';
-import {useEffect , useState} from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Home from "../Home/Home";
-import ListFiles from '../ListFiles/ListFiles';
-import ListClients from '../ListClients/ListClients'
+import ListFiles from "../ListFiles/ListFiles";
+import ListClients from "../ListClients/ListClients";
 import firebase from "../../utils/config";
-import './App.css';
+import "./App.css";
 import today from "../../utils/dateUtils";
 
-function useClients(sortDate=today,sortNurse="",isChange) {
+
+function useClients(sortDate = today, sortNurse = "", isChange) {
   const [clientList, setClientList] = useState({});
 
-  console.log(sortDate)
-  console.log(sortNurse)
-  console.log(isChange)
-  useEffect(() => {
 
-    const fetchData = async()=>{
-     if(sortNurse !== ""){
-      const db = firebase.firestore()
-      const data = await db.collection("clients")
-      .where("dateoftesting", "==", sortDate).where("nurse", "==", sortNurse).get()
-      setClientList(data.docs.map(doc => ({...doc.data(), id: doc.id})))
-     }
-     else{
-      const db = firebase.firestore()
-      const data = await db.collection("clients")
-      .where("dateoftesting", "==", sortDate).get()
-      setClientList(data.docs.map(doc => ({...doc.data(), id: doc.id})))
-     }
-    }
-      
-    fetchData()
-  }, [sortDate,sortNurse,isChange]);
+  console.log(sortDate);
+  console.log(sortNurse);
+  console.log(isChange);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (sortNurse !== "") {
+        const db = firebase.firestore();
+        const data = await db
+          .collection("clients")
+          .where("dateoftesting", "==", sortDate)
+          .where("nurse", "==", sortNurse)
+          .orderBy('rb','asc')
+          .get();
+        setClientList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      } else {
+        const db = firebase.firestore();
+        const data = await db
+          .collection("clients")
+          .where("dateoftesting", "==", sortDate)
+          .get();
+        setClientList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      }
+    };
+
+    fetchData();
+  }, [sortDate, sortNurse, isChange]);
 
   return clientList;
 }
 
 function App() {
-
-  
-
-
   return (
     <Router>
-      
-      <Route exact path='/' component = {Home} />
-      <Route exact path='/listabriseva' render={()=>(<div><ListFiles useClients={useClients}/></div>)} />
-      <Route exact path='/listaracuna' render={()=>(<div><ListClients useClients={useClients} /></div>)} />
-      
+      <Route exact path="/" component={Home} />
+      <Route
+        exact
+        path="/listabriseva"
+        render={() => (
+          <div>
+            <ListFiles useClients={useClients} />
+          </div>
+        )}
+      />
+      <Route
+        exact
+        path="/listaracuna"
+        render={() => (
+          <div>
+            <ListClients useClients={useClients} />
+          </div>
+        )}
+      />
     </Router>
   );
 }
